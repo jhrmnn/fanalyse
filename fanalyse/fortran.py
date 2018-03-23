f25f543f14e34468240fef2bbc82cd985a906248
Jan Hermann <dev@janhermann.cz>
2018-03-23 23:34:56 +0100

    collect used names in program unit bodies

--- fanalyse/fortran.py
+++ fanalyse/fortran.py
@@ -2,15 +2,100 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 import pkg_resources
+from functools import reduce
+from operator import or_
+from abc import ABCMeta, abstractmethod
 
 from textx.metamodel import metamodel_from_file
 
+
+class ASTNode(metaclass=ABCMeta):
+    def __init__(self, **kwargs):
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+        self._used = self.get_used()
+
+    def __repr__(self):
+        if hasattr(self, 'name'):
+            return f'<{self.__class__.__name__}:{self.name}>'
+        return f'<textx:{self.__class__.__name__} instance at {hex(id(self))}>'
+
+    @abstractmethod
+    def get_used(self): ...
+
+
+def _uu(iterable):
+    return reduce(
+        or_,
+        (getattr(x, '_used', set()) for x in iterable),
+        set()
+    ) if iterable else set()
+
+
+def _u(x):
+    return getattr(x, '_used', set())
+
+
+classes = [
+    ('ProgramUnit', lambda self: _uu(self.body)),
+
+    ('CallStatement', lambda self: _u(self.call)),
+    ('IfBlock', lambda self: _uu(self.branches)),
+    ('IfStatement', lambda self: _u(self.condition) | _u(self.body)),
+    ('SelectBlock', lambda self: _u(self.condition) | _uu(self.branches)),
+    ('DoBlock', lambda self: _u(self.condition) | _u(self.from_) |
+     _u(self.to) | _u(self.by) | _uu(self.body)),
+    ('DoStatement', lambda self: _u(self.from_) | _u(self.to) | _u(self.by)),
+    ('ForallBlock', lambda self: _uu(self.iters) | _uu(self.body)),
+    ('ForallStatement', lambda self: _uu(self.iters) | _u(self.body)),
+    ('WhereBlock', lambda self: _u(self.where) | _uu(self.body) |
+     _uu(self.elsebody)),
+    ('WhereStatement', lambda self: _u(self.where) | _u(self.body)),
+    ('AssociateStatement', lambda self: _uu(self.aliases) | _uu(self.body)),
+    ('AllocateStatement', lambda self: _uu(self.variables)),
+    ('DeallocateStatement', lambda self: _uu(self.variables)),
+    ('NullifyStatement', lambda self: _uu(self.variables)),
+    ('OpenStatement', lambda self: _uu(self.parameters)),
+    ('WriteStatement', lambda self: _uu(self.parameters) | _uu(self.args)),
+    ('PrintStatement', lambda self: _uu(self.args)),
+    ('ReadStatement', lambda self: _uu(self.args)),
+    ('CloseStatement', lambda self: _uu(self.parameters)),
+    ('Backspace', lambda self: _u(self.unit)),
+    ('Rewind', lambda self: _u(self.unit)),
+    ('PointerAssignment', lambda self: _u(self.pointer) | _u(self.target)),
+    ('Assignment', lambda self: _u(self.target) | _u(self.value)),
+    ('ControlStatement', None),
+
+    ('Alias', lambda self: _u(self.target)),
+    ('Branch', lambda self: _u(self.condition) | _uu(self.body)),
+    ('DefaultBranch', lambda self: _uu(self.body)),
+    ('Case', lambda self: _uu(self.case) | _uu(self.body)),
+    ('CaseSlice', lambda self: _u(self.from_) | _u(self.to)),
+    ('IterSpec', lambda self: _u(self.variable) | _u(self.slice)),
+
+    ('Expression', lambda self: _uu(self.operands)),
+    ('LHS', lambda self: {self.components[0].variable} | _uu(self.components)),
+    ('Call', lambda self: _u(self.slice) | _uu(self.arguments)),
+    ('Slice', lambda self: _u(self.from_) | _u(self.to) | _u(self.by)),
+    ('KeywordValue', lambda self: _u(self.value)),
+    ('Array', lambda self: _uu(self.elements)),
+    ('Iterator', lambda self: _u(self.from_) | _u(self.to) | _u(self.by) |
+     _uu(self.values)),
+    ('UnaryTerm', lambda self: _u(self.operand)),
+    ('Parenthesised', lambda self: _u(self.expression)),
+
+]
+classes = [
+    type(name, (ASTNode,), {'get_used': f or (lambda self: set())})
+    for name, f in classes
+]
 
 fortran_mm = metamodel_from_file(
     pkg_resources.resource_filename(__name__, 'fortran.tx'),
     ignore_case=True,
     ws='\t ',
     memoization=True,
+    classes=classes,
 )
 
 
