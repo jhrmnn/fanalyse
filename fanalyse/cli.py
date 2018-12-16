2225b2806af893d8b1f55e529d090ed7bcc86449
Jan Hermann <dev@janhermann.cz>
2018-12-16 18:00:00 +0100

    support source preprocessing

--- fanalyse/cli.py
+++ fanalyse/cli.py
@@ -2,6 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 import os
+import subprocess
 from argparse import ArgumentParser
 from multiprocessing import Pool
 from pathlib import Path
@@ -22,9 +23,14 @@ class MyKeyboardInterrupt(Exception):
     pass
 
 
-def parse_source(filename: str) -> Any:
+def parse_source(filename: str, preprocessor: str = None) -> Any:
     try:
-        source = Path(filename).read_text()
+        if preprocessor:
+            source = subprocess.run(
+                f'{preprocessor} {filename}', shell=True, capture_output=True
+            ).stdout.decode()
+        else:
+            source = Path(filename).read_text()
         model = fortran_mm.model_from_str(source + '\n')
         model_dict = model_to_dict(model)  # type: ignore
     except KeyboardInterrupt as e:
@@ -51,14 +57,14 @@ class Collector:
         print(self, end='', flush=True)
 
 
-def parse(filenames: List[str], main: str = None, jobs: int = None) -> None:
+def parse(filenames: List[str], jobs: int = None, preprocessor: str = None) -> None:
     if len(filenames) == 1:
-        parse_source(filenames[0])
+        parse_source(filenames[0], preprocessor)
         return
     parsed = Collector(len(filenames))
     pool = Pool(jobs)
     for filename in filenames:
-        pool.apply_async(parse_source, (filename,), callback=parsed.update)
+        pool.apply_async(parse_source, (filename, preprocessor), callback=parsed.update)
     pool.close()
     pool.join()
 
@@ -67,8 +73,8 @@ def parse_cli() -> Dict[str, Any]:
     parser = ArgumentParser()
     arg = parser.add_argument
     arg('filenames', metavar='FILE', nargs='+')
-    arg('-m', '--main')
     arg('-j', '--jobs', type=int, help=f'default: {os.cpu_count()}')
+    arg('-P', '--preprocessor')
     return vars(parser.parse_args())
 
 
